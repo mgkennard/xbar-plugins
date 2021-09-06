4ab8e46d553d110e0f1bf5daf682a6ffb2aef125
Matthew Kennard <mgkennard@appsonthemove.com>
2021-09-06 16:14:29 +0100

    Initial check-in of xbar plugin to show current clocked in task

---
+++ orgclocking.30s.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# <xbar.title>Show current Org mode clocked-in entry</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Matthew Kennard</xbar.author>
+# <xbar.author.github>appsonthemove</xbar.author.github>
+# <xbar.desc>If you use Org mode clocking to track time against tasks then this plugin will show you the current clocked in task and the elapsed time</xbar.desc>
+
+function client {
+    /usr/local/bin/emacsclient --eval "$1"
+}
+
+if [[ `client "(org-clock-is-active)"` == "nil" ]] ; then
+    echo "No Task"
+else
+    echo -n '⏱'
+    client "(org-clock-get-clock-string)" | head -n 1 | cut -d'"' -f2
+fi
